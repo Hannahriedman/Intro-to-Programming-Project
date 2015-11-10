8e588a5c3a4532b96ba5132b7d9d24c9186ed5cc
Hannah Riedman <hannah.riedman1@marist.edu>
2015-11-09 19:36:12 -0500

    added help function and button.

--- hannahgame.js
+++ hannahgame.js
@@ -10,11 +10,11 @@ var trackerS = 0;
 var trackerT = 0;
 var trackerSh = 0;
 var trackerB = 0;
-var item1 = '';
-var item2 = '';
-var item3 = '';
+var knife = '';
+var banana = '';
+var water = '';
 var inventory1 = ['Waterbottle'];
-var breadcrumbTrail = [];
+var breadcrumbTrail = ['Beach'];
 
 function setting(descrip) {
     document.getElementById("scene").innerHTML = descrip;
@@ -306,32 +306,32 @@ function take() {
     switch (currentLocation) {
         case 'Shack':
                 if (trackerSh === 1) {
-                item1 = 'Knife';
-                inventory1.push(item1);
-                extraInfo('You have taken a '+item1);
+                knife = 'Knife';
+                inventory1.push(knife);
+                extraInfo('You have taken a '+knife);
                 trackerSh = 2;
                 } else {
-                extraInfo('You have already taken the ' + item1);
+                extraInfo('You have already taken the ' + knife);
                 }
                 break;
         case 'BananaTree':
                 if (trackerB === 1) {
-                item2 = 'Banana';
-                inventory1.push(item2);
-                extraInfo('You have taken a '+item2);
+                banana = 'Banana';
+                inventory1.push(banana);
+                extraInfo('You have taken a '+banana);
                 trackerB = 2;
                 } else {
-                extraInfo('You have already taken the ' + item2);
+                extraInfo('You have already taken the ' + banana);
                 }
                 break;
         case 'Waterfall':
                 if (trackerW === 1) {
-                item3 = 'Water';
-                inventory1.push(item3);
-                extraInfo('You have taken a '+item3);
+                water = 'Water';
+                inventory1.push(water);
+                extraInfo('You have taken a '+water);
                 trackerW = 2;
                 } else {
-                extraInfo('You have already taken the ' + item3);
+                extraInfo('You have already taken the ' + water);
                 }
                 break;
         default:
@@ -344,110 +344,22 @@ function inventory() {
 }
 
 function help() {
-    
+    var north = isEnabled('N');
+    var east = isEnabled('E');
+    var south = isEnabled('S');
+    var west = isEnabled('W');
+    extraInfo('Vaild text commands: H,T,I,P,'+north+','+east+','+south+','+west);
 }
-
+function isEnabled(btn) {
+        var blank = ' ';
+        if (document.getElementById(btn).disabled === false) {
+        return btn;
+        } else {
+        return blank;
+        }
+}
 function previous() {
-    
-}
-
-// functions for locations
-function beach(){
-    var message = 'You are now back at the Beach.';
-    enable('W'); // go to cliffs
-    enable('S'); // go to ocean
-    enable('E'); // go to cave
-    enable('N'); // go to jungle
-    return message;
-}
-function cave() {
-    var message = 'You are now in a cave.There is a path to the North and to the South. ';
-    enable('W'); // back to beach
-    enable('S'); // go to waterfall
-    disable('E');
-    enable('N'); // message to dead end
-    return message;
-}
-function cliffs() {
-    var message = 'You are now at the rocky cliffs! Be careful!';
-    enable('W'); // death
-    enable('S'); // go to secretcave
-    enable('E'); // back to beach
-    enable('N'); // message 
-    return message;
-}
-function secretCave() {
-    var message = 'The cliffs drop off into the ocean to the south.' + 
-'You decide to climb down the cliffs and you find an enterance to a cave.';
-    disable('W');
-    disable('S');
-    enable('E'); // go to waterfall
-    enable('N'); // back to cliffs
-    return message;
-}
-function ocean() {
-    var message = 'You are now facing the vast ocean. You do not ' + 
-'have a boat  and it looks pretty dangerous.';
-    enable('W'); // death
-    enable('S'); // death
-    disable('E');
-    enable('N'); // back to beach
-    return message;
-}
-function jungle() {
-    var message ='You are now in the Jungle.';
-    enable('W'); // go to tree
-    enable('S'); // back to beach
-    enable('E'); // go to bananatree
-    enable('N'); // go to trap
-    return message;
-} 
-function waterfall() {
-    var message = 'You are at a Waterfall! Congrats! You have found fresh water! Take some water!';
-    disable('W');
-    disable('S');
-    disable('E');
-    enable('N'); // back to cave enterance
-    return message;
-}
-function trap() {
-    var message = 'You walk through the Jungle for an hour you decide ' +
-'to take a break under a tree.\n After a few moments you notice you ' + 
-'are sinking into the ground. There is a branch to the west and a vine to the East.';
-    enable('W'); // escape trap
-    enable('S'); // death
-    enable('E'); // death
-    enable('N'); // death
-    return message;
-}
-function tree() {
-    var message = 'After a 5 minute walk you come upon a large tree. ' + 
-                  'You climb ontop of it and see what looks like a waterfall in the distance. ' + 
-                  'it appears to be to the east of the beach.\n';
-    disable('W');
-    disable('S');
-    enable('E'); // back to jungle entrance
-    disable('N'); 
-    return message;
-    
-}
-function shack() {
-    var message = 'You grab onto the branch and pull yourself ' + 
-                  'out of the quicksand. You walk further into the jungle before ' +
-                  'discovering a shack. It looks very creepy.There is an old knife there.';
-    disable('W');
-    enable('S'); // go to tree
-    enable('E'); // go back to jungle entrance 
-    disable('N');
-    return message;
-}
-function bananaTree() {
-    var message = 'Yay! You have found some bananas!  Take some, you might be hungry later!';
-    enable('W'); // back to jungle entracne 
-    disable('S');
-    disable('E');
-    disable('N');
-    return message;
+    extraInfo('History: ' + breadcrumbTrail);
 }
 
 // functions for enable/disable buttons
