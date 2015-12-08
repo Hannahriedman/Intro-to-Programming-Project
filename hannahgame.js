6242a530ecee0b97f3da0e133cd889b4d73d65d1
Hannah Riedman <hannah.riedman1@marist.edu>
2015-12-08 18:05:13 -0500

    added in death function and dead location to reset player after they die.

--- hannahgame.js
+++ hannahgame.js
@@ -5,11 +5,12 @@ const EAST = 1;
 const SOUTH = 2;
 const WEST = 3;
 // Prototypes 
-function Location(name, item, tracker, descrip, btn) {
+function Location(name, item, tracker, descrip, alive) {
     this.place = name;
     this.items = item;
     this.beenVisted = tracker;
     this.whatIsHere = descrip;
+    this.endOfGame = alive;
 };
 function Item(name, descrip) {
     this.object = name;
@@ -27,43 +28,43 @@ var FedExBox = new Item('FedEx Box', 'You have taken the FedEx Box.');
 var noItem = new Item('No item', 'There is no item here');
 
 // loctaion objects
-var beach1 = new Location('Beach',noItem,1,'You are now back at the Beach.');
+var beach1 = new Location('Beach',noItem,1,'You are now back at the Beach.',0);
 
-var jungle1 = new Location('Jungle',noItem,0,'You are now in the Jungle.');
+var jungle1 = new Location('Jungle',noItem,0,'You are now in the Jungle.',0);
                            
 var cave1 = new Location('Cave',noItem,0,'You are now in a cave.' + 
-                         ' There is a path to the North and to the South.');
+                         ' There is a path to the North and to the South.',0);
 
 var cliffs1 = new Location('Cliffs',noItem,0,'You are now at the rocky cliffs!' + 
-                           ' Be careful!');
+                           ' Be careful!',0);
 
 var secretCave1 = new Location('SecretCave',waterbottle,0,'The cliffs drop off into the' + 
                                ' ocean to the south. You decide to climb down the cliffs' +
-                               ' and you find an enterance to a cave.');
+                               ' and you find an enterance to a cave.',0);
 
 var waterfall1 = new Location('Waterfall',water,0,'You are at a Waterfall! Congrats!' + 
-                              ' You have found fresh water!');
+                              ' You have found fresh water!',0);
 
 var ocean1 = new Location('Ocean',FedExBox,0,'You are now facing the vast ocean.' + 
-                          ' You do not have a boat  and it looks pretty dangerous.');
+                          ' You do not have a boat  and it looks pretty dangerous.',0);
 
 var trap1 = new Location('Trap',noItem,0,'You walk through the Jungle for an hour' +  
                          'you decide to take a break under a tree.\n After a few' + 
                          'moments you notice you are sinking into the ground.' + 
-                         'There is a branch to the west and a vine to the East.');
+                         'There is a branch to the west and a vine to the East.',0);
 
 var tree1 = new Location('Tree',noItem,0,'After a 5 minute walk you come upon a' + 
                          ' large tree. You climb ontop of it and see what looks' + 
                          ' like a waterfall in the distance. it appears to be' + 
-                         ' to the east of the beach.\n');
+                         ' to the east of the beach.\n',0);
 
 var shack1 = new Location('Shack',knife,0,'You grab onto the branch and pull' + 
                           ' yourself out of the quicksand. You walk further into' + 
-                          ' the jungle before discovering a shack. It looks very creepy.');
+                          ' the jungle before discovering a shack. It looks very creepy.',0);
 
 var bananaTree1 = new Location('BananaTree',banana,0,'You have found Banana Trees,' +
-                               ' there might be monkeys around. ');
-//var dead = new Location('Death',noItem,1,dead());
+                               ' there might be monkeys around. ',0);
+var dead = new Location('Death',noItem,1,'You are dead.',1);
 
 // locations array 
 var locations = [beach1,jungle1,cave1,cliffs1,secretCave1,waterfall1,ocean1,trap1,tree1,shack1,bananaTree1,dead];
@@ -79,7 +80,7 @@ var messageC = 'You walk for 20 minutes but come up to a dead end.' +
               ' There is no water around so you walk back to the entrance of the cave. ';
 var messageCl = 'You can see the whole island from here.' + 
                     ' To the north you see a Jungle, maybe there will be water or food there.' +
-                    ' You go back to the entrance of Cliffs.\n');
+                    ' You go back to the entrance of Cliffs.\n';
 
 var map = [
      // NORTH, EAST, SOUTH, WEST 
@@ -89,12 +90,12 @@ var map = [
      [ messageCl, locations[0], locations[4], null ], // from Cliffs: -Beach,SecretCave-
      [ locations[3], locations[5], null, null ], // from SecretCave: Cliffs,Waterfall--
      [ locations[2], null, null, null ], // from Waterfall: Cave---
-     [ locations[0], null, null, null ], // from Ocean: Beach---
-     [ null, null, null, locations[9] ], // from Trap: ---Shack
+     [ locations[0], dead, dead, null ], // from Ocean: Beach---
+     [ dead, dead, dead, locations[9] ], // from Trap: ---Shack
      [ null, locations[1], null, null ], // from Tree: -Jungle--
      [ null, locations[1], locations[8], null ], // from Shack: -Jungle,Tree-
-     [ null, null, null, locations[1] ] // from BananaTree: ---Jungle
-    
+     [ null, null, null, locations[1] ], // from BananaTree: ---Jungle
+     [ locations[1], locations[2], locations[6], locations[3]]// from death: back to beach 
 ];
 
 
@@ -104,7 +105,7 @@ function showScene(site) {
     buttons(site);
     //document.getElementById("scene").style.backgroundImage = "url('"+site.image+"')";
     yourPoints('Current Points: ' + player.currentPoints);
-    player.breadcrumbTrail.push(player.currentLocation.place);
+    inventory();
     previous();
     
 }
@@ -149,8 +150,10 @@ function move(dir) {
     var nextLocation = from(player.currentLocation,dir);
     if (typeof nextLocation === 'object'){
        if (nextLocation !== null) {
+            death(nextLocation,dir);
             player.currentLocation = nextLocation;
             points();
+            player.breadcrumbTrail.push(player.currentLocation.place);
             showScene(player.currentLocation);
         } else {
             setting("You cannont go that way");
@@ -160,7 +163,6 @@ function move(dir) {
     }
     
 }
-
 
 function points() {
     var place = player.currentLocation;
@@ -279,6 +281,41 @@ function useItem() {
             extraInfo('You do not have the ' + input + 'with you.')
     }
     
+}
+
+function death(nxtlocation, dir) {
+    var nextlocation = nxtlocation;
+    var restart = 'You are dead and restart on the Beach.';
+    if (nextlocation.endOfGame === 1) {   
+        switch(player.breadcrumbTrail.pop()) {
+        case 'Ocean':
+                if (dir === EAST) {
+                    locations[11].whatIsHere = 'You start to swim and get caught in a rip tide.' +
+                    restart;
+                } else {
+                    locations[11].whatIsHere = 'You start to swim and get caught in a rip tide.' +
+                    restart;
+                } 
+                break;
+        case 'Trap':
+                if (dir === EAST) {
+                    locations[11].whatIsHere = 'You reach for the vine but it turns out that its a Snake!' + 
+                    ' Still trapped by the quicksand, the snake attacks and you die.' + restart;
+                } else {
+                    locations[11].whatIsHere = 'You try to go that way but you end up sinking further ' + 
+                    ' into the quicksand.' + restart;
+                }
+            break;
+        case 'Cliffs':
+            locations[11].whatIsHere = 'You should have been more careful. You try to ' + 
+            'get a better look at the edge of the cliffs and you fall to your death.' + restart;
+            break;
+        }
+        player.currentPoints = 0;
+        player.currentLocation = locations[0];
+        player.inventory = [];
+        player.breadcrumbTrail = ['Beach'];
+    } 
 }
 
 // functions for enable/disable buttons
